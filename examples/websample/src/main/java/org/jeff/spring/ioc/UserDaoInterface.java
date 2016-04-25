ca74db9b9090d998666b0ea217e9ac9bb8fb4adf
liqiangqiang <liqiangqiang@youzan.com>
2016-04-25 20:50:57 +0800

    sd delete spring.ioc and examples

--- examples/websample/src/main/java/org/jeff/spring/ioc/UserDaoInterface.java
+++
@@ -1,5 +0,0 @@
-package org.jeff.spring.ioc;
-
-public interface UserDaoInterface {
-	public void save(String userName);
-}
--- examples/websample/src/test/java/org/jeff/spring/proxy/Hello.java
+++
@@ -1,8 +0,0 @@
-package org.jeff.spring.proxy;
-
-/**
- * Created by BYSocket on 2015/11/16.
- */
-public interface Hello {
-    public void say(String name);
-}
